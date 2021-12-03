9eb1073e918fa8e6bd0b6ee0167fcee26d625cda
JordyTheCoder <87208598+JordyTheCoder@users.noreply.github.com>
2021-12-02 19:16:56 -0500

    Actually completed Day 2

--- Day 2/day2.js
+++ Day 2/day2.js
@@ -4,18 +4,31 @@ const {readFileSync} = require("fs");
 let positionsFile = readFileSync("day2.txt", "utf8").replace(/\r/g, "").split(/[' '\n]/)
 let horizantal = 0;
 let depth = 0;
-console.debug(positionsFile)
 
 let positions = positionsFile.map(i => {
     if(i.length === 1) return Number(i);
     else return i;
 })
-console.debug(positions)
 
 for(let i = 1; i < positions.length; i+=2) {
     let instruct = i - 1;
     if(positions[instruct] === "forward") horizantal += positions[i];
     else if(positions[instruct] === "down") depth += positions[i];
     else depth -= positions[i];
+}
+console.log(horizantal * depth);
+
+//Part 2
+horizantal = 0;
+depth = 0;
+let aim = 0;
+for(let i = 1; i < positions.length; i+=2) {
+    let instruct = i - 1;
+    if(positions[instruct] === "forward") {
+        horizantal += positions[i];
+        depth += (aim * positions[i]);
+    }
+    else if(positions[instruct] === "down") aim += positions[i];
+    else aim -= positions[i];
 }
 console.log(horizantal * depth)
